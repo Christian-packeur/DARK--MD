f36f3f281a90936d5afe718315fd262755703959
Christian-packeur <ndaguessendavid@gmail.com>
2025-01-27 07:17:41 +0000

    panel.js

--- panel.js
+++ panel.js
@@ -1,12 +1,48 @@
-On présente des outils informatiques gratuits et facile à utiliser même sans connaissance 
+const { exec } = require("child_process");
+const fs = require('fs');
+const path = require('path');
 
-* bot whatsapp 
-* panel free 
-* AI ( intelligence artificielle )
-* whatsapp mod 
-* termux tools
-* un peu du hacking 
+async function startApplication() {
+  console.log("Salut👋 !!! Vous êtes sur le point de déployer DARK--MD sur le panel. Incroyable n'est ce pas chanceux !");
 
-Et plein d'autre encore
+  // Vérifier si le fichier set.env existe
+  const envFilePath = path.join(__dirname, "set.env");
+  if (!fs.existsSync(envFilePath)) {
+    console.log("Le fichier set.env n'a pas été trouvé. Veuillez créer le fichier et y ajouter vos variables.");
+    return;
+  }
 
-https://whatsapp.com/channel/0029VaiPkRPLY6d0qEX50e2k
+  // Lire le fichier set.env pour voir si la SESSION est la...
+  const envFileContent = fs.readFileSync(envFilePath, "utf-8");
+  const envVariables = envFileContent.split("\n").reduce((acc, line) => {
+    const [key, value] = line.split("=");
+    if (key && value) {
+      acc[key.trim()] = value.trim();
+    }
+    return acc;
+  }, {});
+
+  // Vérifier la valeur de SESSION_ID
+  if (envVariables.SESSION_ID === "zokk") {
+    console.log("Veuillez mettre votre SESSION_ID dans le fichier set.env.");
+    return;
+  }
+
+  // Démarrer l'application avec PM2
+  const childProcess = exec("npx pm2 start index.js --attach");
+
+  childProcess.stdout.on("data", data => {
+    console.log(data);
+  });
+
+  childProcess.stderr.on("data", error => {
+    console.error(error);
+  });
+
+  childProcess.on("close", code => {
+    console.log(`Processus stoppe avec le code : ${code}`);
+  });
+}
+
+// Démarrer l'application et gérer les erreurs
+startApplication().catch(console.error);
